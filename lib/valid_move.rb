315ee0a8a718108907f4814da82369c7de97da4d
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2019-08-09 16:38:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -27,8 +27,7 @@ end
 def valid_move?(board, position)
   if position.between?(0, 8)
     return true
-  elsif board[position] != "X" || board[position] != "O"
-    return true
+  
   else 
     return false
   end 
