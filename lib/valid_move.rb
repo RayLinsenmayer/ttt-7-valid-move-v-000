e64b064feb51381c73531bb5bda9013d2b5efdb6
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2019-08-09 15:44:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -22,7 +22,4 @@ def position_taken?(board, position)
   else return true
 end
 end
-code your #valid_move? method here
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
