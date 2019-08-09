9b6efddff4ab089cf3b7eb11fe88cd1759e2b8b0
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2019-08-09 15:56:38 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -17,14 +17,16 @@ def move(board, position, char = "X")
 end
 
 def valid_move?(board_position)
-  if board[position] <1 || board[position]>9
-    return 
-  else return true
-  end
+  if board[position] <1 || board[position] >9
+    return false  
+  else 
+    return true
+  end  
   def position_taken?(board, position)
     if board[position] == " " || board[position]== "" || board[position] == nil
-    return false
-  else return true
+      return false
+    else 
+      return true
+    end
   end
 end
-
